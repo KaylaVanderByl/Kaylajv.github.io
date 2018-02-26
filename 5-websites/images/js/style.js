d6bb0f4d9585f5cddea4beef1c06fa5a61feb7a2
Kayla <vandk438@newschool.edu>
2018-02-25 21:17:18 -0500

    webs

---
+++ 5-websites/images/js/style.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+function myFunction() {
+    var x = document.getElementById("Demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
