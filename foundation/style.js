1af2a3a3af33feed019320570f5ea923bdf26942
Kayla <vandk438@newschool.edu>
2018-03-10 13:53:43 -0500

    dd

--- foundation/style.js
+++ foundation/style.js
@@ -16,27 +16,3 @@ window.onclick = function(event) {
     }
   }
 }
-
-$(".twitter").hover(function () {
-	$("#social-section").toggleClass("color-twitter")
-});
-
-$(".facebook").hover(function () {
-	$("#social-section").toggleClass("color-facebook")
-});
-
-$(".googleplus").hover(function () {
-	$("#social-section").toggleClass("color-googleplus")
-});
-
-$(".pinterest").hover(function () {
-	$("#social-section").toggleClass("color-pinterest")
-});
-
-$(".dribbble").hover(function () {
-	$("#social-section").toggleClass("color-dribbble")
-});
-
-$(".instagram").hover(function () {
-	$("#social-section").toggleClass("color-instagram")
-});
