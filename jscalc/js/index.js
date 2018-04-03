620321fc9fb9870425237b8b163a3811fa07cd16
Kayla <vandk438@newschool.edu>
2018-04-03 14:32:53 -0400

    ee

---
+++ jscalc/js/index.js
@@ -0,0 +1,8 @@
+function multiplyBy()
+{
+  var result= document.getElementById("result");
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("secondNumber").value;
+  result.innerHTML = num1 * num2;
+
+}
