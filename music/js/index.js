5ee13f0e6f76df4525ac73d98937bcdac7315880
Kayla <vandk438@newschool.edu>
2018-04-22 20:39:17 -0400

    asa

--- music/js/index.js
+++ music/js/index.js
@@ -7,3 +7,10 @@ jQuery('.targetDiv').hide();
               jQuery('#div'+$(this).attr('target')).show();
         });
 });
+
+var cpo = []; cpo["_object"] ="cp_widget_f0ee1bac-2fb8-41f3-bc11-4bb061785ed0"; cpo["_fid"] = "AsMAIMeFty42";
+var _cpmp = _cpmp || []; _cpmp.push(cpo);
+(function() { var cp = document.createElement("script"); cp.type = "text/javascript";
+cp.async = true; cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
+var c = document.getElementsByTagName("script")[0];
+c.parentNode.insertBefore(cp, c); })();
--- stud/style.js
+++ stud/style.js
@@ -5,3 +5,27 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+$(function() {
+
+
+  $('#contact').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target)
+        && container.has(e.target).length === 0)
+    {
+        container.fadeOut();
+    }
+  });
+
+
+$('#boxclose').click(function(){
+      $('#contactForm').animate({'top':'-200px'},500,function(){
+          $('#contactForm').fadeOut('fast');
+      });
+  });
+
+});
